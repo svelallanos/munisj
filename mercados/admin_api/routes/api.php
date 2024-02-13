07c908b500d363052b1547ab27c2023508009da2
svelallanos <svelallanos@gmail.com>
2024-02-13 09:35:01 -0500

    ggg

--- mercados/admin_api/routes/api.php
+++ mercados/admin_api/routes/api.php
@@ -21,4 +21,5 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
     Route::get('me', 'me');
+    Route::delele('deleteUser', 'deleteUser');
 });
